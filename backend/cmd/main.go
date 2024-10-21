8dd34fca37ccc8ccbee787df460cb51513ef1af4
Phantom <wayneb.0210@gmail.com>
2024-10-21 17:30:12 +0800

    use the cors middleware for communication between frontend and backend on different port

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"Todo-go-htmx/internal/handler"
-
+	"Todo-go-htmx/internal/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
 
-	// r.Static("/static","/web/static")
-
-	// r.GET("/", func(c * gin.Context){
-	// 	c.File("./web/static/index.html")
-	// })
+	// Cors Middleware
+	r.Use(middleware.CORSConfig())
 	r.GET("/todos", handler.GetTodos)
 	r.GET("/todos/:id", handler.GetTodosById)
 	r.POST("/todos", handler.CreateTodo)     
